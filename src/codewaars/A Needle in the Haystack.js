c6809b53b8efe8822dc60480c4ee8f29e7750cbf
pashkouski <satiym@gmail.com>
2023-01-14 16:53:55 +0300

    25

---
+++ src/codewaars/A Needle in the Haystack.js
@@ -0,0 +1,4 @@
+function findNeedle(haystack) {
+    return `found the needle at position ${haystack.indexOf('needle')}`;
+}
+//https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/javascript
